896985241d5eb25b084fb38ba9955ed869def168
Danny Parker <danny.parker@myunidays.com>
2018-07-31 14:48:53 +0100

    update codeless client to return datetime

--- test/test-codeless.js
+++ test/test-codeless.js
@@ -1,33 +1,33 @@
 const assert = require('assert'),
-      Codeless = require('../lib/codeless.js');
+    Codeless = require('../lib/codeless.js');
 
 const sharedSecret = 'tnFUmqDkq1w9eT65hF9okxL1On+d2BQWUyOFLYE3FTOwHjmnt5Sh/sxMA3/i0od3pV5EBfSAmXo//fjIdAE3cIAatX7ZZqVi0Dr8qEYGtku+ZRVbPSmTcEUTA/gXYo3KyL2JqXaZ/qhUvCMbLWyV07qRiFOjyLdOWhioHlJM5io=',
-      studentId = 'Do/faqh330SGgCnn4t3X4g==',
-      timestamp = 1395741712,
-      hash = 'i38dJdX+XLKuE4F5tv+Knpl5NPtu5zrdsjnqBQliJEJE4NkMmfurVnUaT46WluRYoD1/f5spAqU36YgeTMCNeg==';
+    studentId = 'Do/faqh330SGgCnn4t3X4g==',
+    timestamp = 1395741712,
+    hash = 'i38dJdX+XLKuE4F5tv+Knpl5NPtu5zrdsjnqBQliJEJE4NkMmfurVnUaT46WluRYoD1/f5spAqU36YgeTMCNeg==';
 
-describe('Codeless', function() {
-  const codeless = new Codeless(sharedSecret);
+describe('Codeless', function () {
+    const codeless = new Codeless(sharedSecret);
 
-  describe('#validate', function() {
-    it('should validate hash', function() {
-      const result = codeless.validate(studentId, timestamp, hash);
+    describe('#validate', function () {
+        it('should validate hash', function () {
+            const result = codeless.validate(studentId, timestamp, hash);
 
-      assert.strictEqual(result, true);
+            assert.deepStrictEqual(result, new Date(timestamp));
+        });
+
+        it('should reject invalid hash', function () {
+            const result = codeless.validate(studentId, timestamp, 'garbage');
+
+            assert.strictEqual(result, null);
+        });
     });
 
-    it('should reject invalid hash', function() {
-      const result = codeless.validate(studentId, timestamp, 'garbage');
+    describe('#hash', function () {
+        it('should hash parameters', function () {
+            const result = codeless.hash(studentId, timestamp);
 
-      assert.strictEqual(result, false);
-    });
-  });
-
-  describe('#hash', function() {
-    it('should hash parameters', function() {
-      const result = codeless.hash(studentId, timestamp);
-
-      assert.strictEqual(result, hash);
+            assert.strictEqual(result, hash);
+        })
     })
-  })
 });
